deb85281a807f6d550c64e241919ad29389b9e21
Dmitriy Birilko <101152412+Sauberr@users.noreply.github.com>
2022-12-30 07:20:32 +0300

    Update code.calculator.py

--- code.calculator.py
+++ code.calculator.py
@@ -3,12 +3,5 @@ try:
 except ZeroDivisionError:
     print('На 0 делить нельзя')
     while True: print(eval(input('>>>:')))
-except NameError:
+except (NameError,SyntaxError,TypeError)  :
     print('Введите число а не строку или переменною')
-    while True: print(eval(input('>>>:')))
-except SyntaxError:
-    print('Введите заново число')
-    while True: print(eval(input('>>>:')))
-except TypeError:
-    print('Введите число а не что то другое ')
-    while True: print(eval(input('>>>:')))
